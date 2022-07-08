21db0678471199485d6337ab174db7ab7802ec4a
Ed Carrel <edward@carrel.org>
2022-07-07 23:05:30 -0700

    wip

---
+++ serve.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
