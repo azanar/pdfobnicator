903cd63533cf0aee1aaa9997f91d91aa790edc58
Ed Carrel <azanar@carrel.org>
2023-10-05 23:32:37 +0000

    create a test mode!

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import { attach, DocumentWells } from './ui/render.jsx'
 
 const root = document.getElementById('wells')
 
-const handles = []
-
+const url = new URL(document.URL)
+console.log(url)
+let handles = []
+if (url.searchParams.get("test")) {
+    console.log("test mode active!")
+    handles = [new UrlFileHandle("sample.pdf"), new UrlFileHandle("sample.pdf")]
+}
 const wells = <DocumentWells handles={handles}/>
 
 attach(root,wells);
